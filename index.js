36d9b46f8a8129da50685823c4ddf082423483f9
shahar amit <shaharamit@shahars-mbp.home>
2018-05-24 02:35:34 +0300

    test12

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use('/assets', express.static(`${__dirname}/def`));
 app.use(
     (req,res,next) => {
         res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.set("Content-Type", "application/json");
         next();
     });
